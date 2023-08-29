5c4d7038eebd487f23d0ca2243c02f62d5e33db3
Tony <totran0331@gmail.com>
2023-08-29 11:53:20 -0400

    Added prismic; worked on landing

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+   plugins: ["prettier-plugin-tailwindcss"],
+   trailingComma: "es5",
+   tabWidth: 3,
+   singleQuote: false,
+   semi: false,
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,182 @@
-import Image from 'next/image'
+"use client"
+
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import TextTransition, { presets } from "react-text-transition"
+
+const LINKS = [
+   {
+      name: "Blog",
+      slug: "/blog",
+   },
+   {
+      name: "Photos",
+      slug: "/photos",
+   },
+   {
+      name: "Contact",
+      slug: "/contact",
+   },
+]
+
+const TITLES = ["a developer", "a designer", "an entrepreneur"]
+
+const RECENT_ARTICLES = [
+   {
+      title: "Article name",
+      slug: "article-one",
+      date: "August 28, 2023",
+   },
+   {
+      title: "Article name 2",
+      slug: "article-two",
+      date: "August 28, 2023",
+   },
+   {
+      title: "Article name 3",
+      slug: "article-one",
+      date: "August 28, 2023",
+   },
+   {
+      title: "Article name 4",
+      slug: "article-one",
+      date: "August 28, 2023",
+   },
+]
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+   const [index, setIndex] = useState(0)
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+   useEffect(() => {
+      const intervalId = setInterval(() => {
+         setIndex((index) => index + 1)
+      }, 5000)
+      return () => clearTimeout(intervalId)
+   }, [])
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+   return (
+      <main className="mx-auto flex min-h-screen max-w-6xl flex-col">
+         <nav className="flex w-full items-center justify-between border-b border-neutral-700/75 p-6 lg:p-8">
+            <h3 className="text-xl font-bold text-white">tt.</h3>
+            <div className="flex items-center space-x-4 text-base lg:space-x-8">
+               {LINKS.map(({ name, slug }) => (
+                  <Link key={name} href={slug}>
+                     <p className="font-medium text-neutral-400">{name}</p>
+                  </Link>
+               ))}
+            </div>
+         </nav>
+         <div className="gap-6 px-6 py-16 lg:grid lg:grid-cols-2 lg:gap-12 lg:px-8 lg:py-24">
+            <div className="">
+               <div className="text-3xl font-bold tracking-wide text-white lg:text-5xl">
+                  <h1 className="">Hello, world! 👋</h1>
+                  <h1 className="mt-4">I&apos;m Tony Tran</h1>
+               </div>
+               <div className="mt-8 space-y-4 text-base leading-8 text-neutral-400">
+                  <div className="">
+                     I&apos;m{" "}
+                     <TextTransition
+                        className="text-sky-400"
+                        inline
+                        springConfig={presets.gentle}
+                     >
+                        {TITLES[index % TITLES.length]}
+                     </TextTransition>{" "}
+                     studying Computer Science at the University of Virginia.
+                  </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                  <p className="">
+                     I love working on projects in the startup, cybersecurity,
+                     finance, and software automation spaces. Right now,
+                     I&apos;m building{" "}
+                     <a
+                        href="https://github.com/tot/musicmates"
+                        className="text-neutral-100 underline"
+                     >
+                        Harmonyze
+                     </a>
+                     , a Linktree-like platform for people to share their music
+                     personalities.
+                  </p>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                  <p className="">
+                     Check out my{" "}
+                     <a href="#" className="text-neutral-100 underline">
+                        blog
+                     </a>{" "}
+                     where I detail my experiences building projects, share
+                     insights about technologies, and jot down thoughts.
+                  </p>
+               </div>
+            </div>
+            <div className="mx-auto mt-12 grid h-[20rem] w-full max-w-full grid-cols-2 grid-rows-2 gap-6 md:max-w-xl lg:mt-0 lg:h-[32rem] lg:max-w-full lg:gap-8">
+               <div className="rounded-lg border-neutral-800/75 bg-neutral-800/30">
+                  location
+               </div>
+               <div className="row-span-2 rounded-lg border-neutral-800/75 bg-neutral-800/30">
+                  portrait
+               </div>
+               <div className="rounded-lg border-neutral-800/75 bg-neutral-800/30">
+                  spotify
+               </div>
+            </div>
+         </div>
+         <div className="px-6 py-8 lg:px-8 lg:py-16">
+            <h1 className="text-3xl font-bold">Writings</h1>
+            <div className="mt-6 divide-y-2 divide-neutral-800/75 border border-neutral-800/75 text-neutral-200">
+               {RECENT_ARTICLES.map(({ title, slug, date }) => (
+                  <div key={slug}>
+                     <Link href="">
+                        <div className="flex w-full items-baseline justify-between p-4 hover:border-neutral-700 hover:bg-neutral-800/25">
+                           <p className="text-lg font-medium">{title}</p>
+                           <time className="font-mono text-indigo-500">
+                              {date}
+                           </time>
+                        </div>
+                     </Link>
+                  </div>
+               ))}
+               {/* <div className="">
+                  <Link href="">
+                     <button
+                        className="flex h-8 items-center justify-center text-base text-neutral-400"
+                        type="button"
+                     >
+                        All posts -{">"}
+                     </button>
+                  </Link>
+               </div> */}
+            </div>
+         </div>
+         <div className="px-6 py-8 lg:px-8 lg:py-16">
+            <h1 className="text-3xl font-bold">Projects</h1>
+            <div className="mt-6 grid grid-cols-2">
+               <div className="flex h-48 flex-col justify-between rounded-lg border-neutral-800/75 bg-neutral-800/20 p-6">
+                  <div className="w-1/2">
+                     <time className="font-mono text-base text-blue-500">
+                        2023
+                     </time>
+                     <p className="text-lg font-semibold leading-5 text-neutral-100">
+                        Harmonyze
+                     </p>
+                     <p className="mt-2 text-sm text-neutral-400">
+                        Description of project
+                     </p>
+                  </div>
+                  <a href="" className="">
+                     <button
+                        className="flex h-8 items-center justify-center rounded-md border border-neutral-700 bg-neutral-800/25 px-2 text-sm hover:bg-neutral-100 hover:text-black"
+                        type="button"
+                     >
+                        View repo
+                     </button>
+                  </a>
+               </div>
+            </div>
+         </div>
+         <div className="px-8 py-24">
+            <h1 className="">Technologies</h1>
+         </div>
+      </main>
+   )
 }
