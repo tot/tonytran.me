9fc60e533b1757e32b473ef395c4244336ca9f1f
Tony <totran0331@gmail.com>
2023-08-30 17:33:58 -0400

    Redesigned articles section

--- src/app/components/home/RecentArticle.tsx
+++ src/app/components/home/RecentArticle.tsx
@@ -16,15 +16,15 @@ const RecentArticle: FC<RecentArticleProps> = ({
 }) => {
    return (
       <Link href={slug}>
-         <div className="grid w-full grid-cols-4 from-transparent to-indigo-800/10 p-4 transition-all duration-150 hover:bg-neutral-800/20 hover:bg-gradient-to-r">
-            <div className="col-span-1">
-               <time className="font-mono text-indigo-500">{date}</time>
-            </div>
-            <div className="col-span-3">
-               <h3 className="text-base font-medium leading-8 tracking-wide">
+         <div className="w-full gap-4 from-transparent to-indigo-800/10 p-4 transition-all duration-150 hover:bg-neutral-800/20 hover:bg-gradient-to-r">
+            <div className="col-span-4">
+               <time className="font-mono text-sm font-semibold text-indigo-500">
+                  {date}
+               </time>
+               <h3 className="mt-2 text-base font-bold leading-8 text-neutral-100">
                   {title}
                </h3>
-               <p className="text-sm leading-6 text-neutral-500">{preview}</p>
+               <p className="text-sm leading-6 text-neutral-400">{preview}</p>
             </div>
          </div>
       </Link>
--- src/app/components/home/Writings.tsx
+++ src/app/components/home/Writings.tsx
@@ -36,34 +36,45 @@ const RECENT_ARTICLES = [
    },
 ]
 
-const Writings = () => {
+const AllPostsButton = () => {
    const [isHover, setHover] = useState(false)
    return (
+      <div className="">
+         <Link href="">
+            <button
+               className="flex h-10 items-center justify-center rounded border border-neutral-800 px-3 text-base text-neutral-300 shadow transition-colors duration-150 hover:bg-neutral-700/25"
+               type="button"
+               onMouseEnter={() => setHover(true)}
+               onMouseLeave={() => setHover(false)}
+            >
+               <span className="text-sm font-medium">All posts</span>
+               <motion.div animate={{ x: isHover ? 4 : 0 }}>
+                  <BiRightArrowAlt className="ml-1 text-xl" />
+               </motion.div>
+            </button>
+         </Link>
+      </div>
+   )
+}
+
+const Writings = () => {
+   return (
       <Section>
-         <span className="flex items-baseline justify-between">
-            <h1 className="text-3xl font-bold text-white">Recent Writings</h1>
-            <div className="">
-               <Link href="">
-                  <button
-                     className="flex h-10 items-center justify-center rounded border border-neutral-800 px-3 text-base text-neutral-100 transition-colors duration-150 hover:bg-neutral-700/25"
-                     type="button"
-                     onMouseEnter={() => setHover(true)}
-                     onMouseLeave={() => setHover(false)}
-                  >
-                     <span className="text-sm font-medium">All posts</span>
-                     <motion.div animate={{ x: isHover ? 4 : 0 }}>
-                        <BiRightArrowAlt className="ml-1 text-xl" />
-                     </motion.div>
-                  </button>
-               </Link>
+         <span className="items-baseline justify-between md:flex">
+            <h1 className="text-3xl font-bold text-white">Recent Articles</h1>
+            <div className="hidden md:block">
+               <AllPostsButton />
             </div>
          </span>
-         <div className="mt-6 divide-y divide-neutral-700/50 overflow-hidden rounded-lg text-neutral-200">
+         <div className="mt-6 divide-y divide-neutral-700/50 overflow-hidden rounded-lg border border-neutral-700/50 text-neutral-200">
             {RECENT_ARTICLES.map((article) => (
                <div key={article.slug}>
                   <RecentArticle key={article.slug} {...article} />
                </div>
             ))}
+         </div>
+         <div className="mt-4 flex justify-center md:hidden">
+            <AllPostsButton />
          </div>
       </Section>
    )
