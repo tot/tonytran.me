4a46595a4114cd53ce08ada5d48faaa4713a4e77
Tony <totran0331@gmail.com>
2023-09-01 02:41:29 -0400

    added about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Blog() {
+   return (
+      <main className="relative mx-auto flex min-h-screen max-w-4xl flex-col">
+         <div className="fixed bottom-0 left-0 right-0 top-0 -z-10 bg-[linear-gradient(to_right,#4f4f4f1A_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f1A_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+      </main>
+   )
+}
