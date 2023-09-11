6f12839f5d21be3e8e2a31a24353e495319576ba
Tony <totran0331@gmail.com>
2023-09-11 01:52:47 -0400

    Redid mobile navbar; fixed desktop navbar positioning

--- src/app/components/DesktopNavbar.tsx
+++ src/app/components/DesktopNavbar.tsx
@@ -3,7 +3,6 @@ import { usePathname } from "next/navigation"
 import { cn } from "@utils/utils"
 import { NavLink } from "@utils/types"
 import { motion } from "framer-motion"
-import { useState } from "react"
 
 interface DesktopNavbarProps {
    links: NavLink[]
@@ -14,18 +13,11 @@ const DesktopNavbar = ({ links }: DesktopNavbarProps) => {
 
    if (currentRoute.includes("/blog/")) currentRoute = "/blog"
 
-   const [hoveredRoute, setHoveredRoute] = useState(currentRoute)
-
    return (
-      <nav className="fixed inset-x-0 top-5 z-50 mx-auto hidden w-fit items-center justify-between rounded-full border border-neutral-700/75 bg-neutral-800/50 px-2 py-2 shadow-[inset_0px_0.0625rem_0_rgba(255,255,255,0.05),0_0.25rem_0.5rem_0_rgba(0,0,0,0.1)] shadow-md filter backdrop-blur-md md:flex lg:top-10">
-         <ul className="flex items-center space-x-2 font-mono">
+      <nav className="fixed inset-x-0 top-5 z-50 mx-auto hidden w-fit items-center justify-between rounded-full border border-neutral-700/75 bg-neutral-800/50 px-2 py-2 shadow-[inset_0px_0.0625rem_0_rgba(255,255,255,0.05),0_0.25rem_0.5rem_0_rgba(0,0,0,0.1)] shadow-md filter backdrop-blur-md md:flex">
+         <ul className="flex items-center space-x-2 font-sans text-sm">
             {links.map(({ name, slug }) => (
-               <Link
-                  key={name}
-                  href={slug}
-                  onMouseOver={() => setHoveredRoute(slug)}
-                  onMouseLeave={() => setHoveredRoute(currentRoute)}
-               >
+               <Link key={name} href={slug}>
                   <motion.li
                      className={cn(
                         "relative rounded-full px-3 py-1 font-medium transition-colors duration-200 hover:text-neutral-100",
@@ -37,9 +29,9 @@ const DesktopNavbar = ({ links }: DesktopNavbarProps) => {
                      whileTap={{ scale: 0.9 }}
                   >
                      <span>{name}</span>
-                     {slug === hoveredRoute && (
+                     {slug === currentRoute && (
                         <motion.div
-                           className="absolute bottom-0 left-0 -z-10 h-full w-full rounded-full bg-gradient-to-b from-neutral-700/40 to-neutral-800/40 shadow-[inset_0px_0.0625rem_0_rgba(255,255,255,0.05),0_0.25rem_0.5rem_0_rgba(0,0,0,0.1)] filter backdrop-blur"
+                           className="absolute bottom-0 left-0 -z-10 h-full w-full rounded-full bg-neutral-700/40 shadow-[inset_0px_0.0625rem_0_rgba(255,255,255,0.05),0_0.25rem_0.5rem_0_rgba(0,0,0,0.1)] filter backdrop-blur"
                            layoutId="navbar"
                            aria-hidden="true"
                            transition={{
--- src/app/components/MobileNavbar.tsx
+++ src/app/components/MobileNavbar.tsx
@@ -5,7 +5,7 @@ import { NavLink } from "@utils/types"
 import { useLockBodyScroll } from "@uidotdev/usehooks"
 import { cn } from "@utils/utils"
 import { Pivot as Hamburger } from "hamburger-react"
-import { BiLogoLinkedin, BiLogoGithub } from "react-icons/bi"
+import { BiLogoLinkedin, BiLogoGithub, BiX } from "react-icons/bi"
 
 const SOCIALS = [
    {
@@ -44,7 +44,11 @@ interface MobileNavbarProps {
    links: NavLink[]
 }
 
-const NavbarBody = ({ links }: MobileNavbarProps) => {
+interface NavbarBodyProps extends MobileNavbarProps {
+   toggleOpen: () => void
+}
+
+const NavbarBody = ({ links, toggleOpen }: NavbarBodyProps) => {
    let currentRoute = usePathname() || "/"
 
    if (currentRoute.includes("/blog/")) currentRoute = "/blog"
@@ -52,55 +56,74 @@ const NavbarBody = ({ links }: MobileNavbarProps) => {
    useLockBodyScroll()
 
    return (
-      <motion.aside
-         initial={{ width: 0 }}
-         animate={{
-            width: "100%",
-         }}
-         exit={{
-            width: 0,
-            transition: { delay: 0.7, duration: 0.3 },
-         }}
-         className="fixed right-0 top-0 z-30 h-full w-full bg-[#0c0b0b]"
-      >
-         <div className="flex h-full flex-col justify-between bg-[#101011]">
+      <div className="fixed z-[60] h-screen w-full bg-transparent">
+         <div className="relative h-full w-full">
             <motion.div
-               className="space-y-2 p-6 pt-8"
-               initial="closed"
-               animate="open"
-               exit="closed"
-               variants={sideVariants}
+               initial={{ opacity: 0 }}
+               animate={{ opacity: 1 }}
+               exit={{ opacity: 0, transition: { delay: 0.7, duration: 0.3 } }}
+               onClick={() => toggleOpen()}
+               className="h-full w-full bg-neutral-900/30 filter backdrop-blur-sm"
+            ></motion.div>
+            <motion.aside
+               initial={{ width: 0 }}
+               animate={{
+                  width: "75%",
+               }}
+               exit={{
+                  width: 0,
+                  transition: { delay: 0.7, duration: 0.3 },
+               }}
+               className="fixed right-0 top-0 z-50 flex h-full"
             >
-               {links.map(({ name, slug }) => (
+               <button
+                  className="mr-6 mt-6 flex h-8 w-8 items-center justify-center rounded-full border-neutral-700/50 bg-[#2e2d2d]/80"
+                  type="button"
+                  onClick={() => toggleOpen()}
+               >
+                  <BiX className="h-6 w-6 text-neutral-100" />
+               </button>
+               <div className="flex h-full flex-1 flex-col justify-between border-l border-neutral-700/50 bg-[#101011]">
                   <motion.div
-                     key={name}
-                     variants={itemVariants}
-                     className={cn(
-                        "block p-4 font-general-sans text-3xl font-bold",
-                        {
-                           "rounded-lg bg-neutral-800/20 bg-gradient-to-r from-neutral-700/50 to-neutral-700/25 text-neutral-100 shadow-[inset_0px_0.0625rem_0_rgba(255,255,255,0.05),0_0.25rem_0.5rem_0_rgba(0,0,0,0.1)]":
-                              currentRoute === slug,
-                           "text-neutral-400": currentRoute !== slug,
-                        }
-                     )}
+                     className="space-y-2 p-6"
+                     initial="closed"
+                     animate="open"
+                     exit="closed"
+                     transition={{ duration: 1 }}
+                     variants={sideVariants}
                   >
-                     <Link href={slug}>{name}</Link>
+                     {links.map(({ name, slug }) => (
+                        <motion.div
+                           key={name}
+                           variants={itemVariants}
+                           className={cn(
+                              "block px-3 py-2 font-general-sans text-lg font-medium",
+                              {
+                                 "rounded-lg bg-neutral-800/20 bg-gradient-to-r from-neutral-700/50 to-neutral-700/25 text-neutral-100 shadow-[inset_0px_0.0625rem_0_rgba(255,255,255,0.05),0_0.25rem_0.5rem_0_rgba(0,0,0,0.1)]":
+                                    currentRoute === slug,
+                                 "text-neutral-400": currentRoute !== slug,
+                              }
+                           )}
+                        >
+                           <Link href={slug}>{name}</Link>
+                        </motion.div>
+                     ))}
                   </motion.div>
-               ))}
-            </motion.div>
 
-            <ul className="mt-auto flex items-center space-x-4 p-6 text-3xl text-neutral-500">
-               {SOCIALS.map(({ icon, url }) => (
-                  <li
-                     key={url}
-                     className="transition-colors duration-150 hover:text-neutral-200"
-                  >
-                     <Link href={url}>{icon}</Link>
-                  </li>
-               ))}
-            </ul>
+                  <ul className="mt-auto flex items-center space-x-4 p-6 text-3xl text-neutral-500">
+                     {SOCIALS.map(({ icon, url }) => (
+                        <li
+                           key={url}
+                           className="transition-colors duration-150 hover:text-neutral-200"
+                        >
+                           <Link href={url}>{icon}</Link>
+                        </li>
+                     ))}
+                  </ul>
+               </div>
+            </motion.aside>
          </div>
-      </motion.aside>
+      </div>
    )
 }
 
@@ -109,13 +132,18 @@ const MobileNavbar = ({ links }: MobileNavbarProps) => {
    return (
       <>
          <AnimatePresence>
-            {isOpen && <NavbarBody links={links} />}
+            {isOpen && (
+               <NavbarBody links={links} toggleOpen={() => toggleOpen()} />
+            )}
          </AnimatePresence>
          <button
             type="button"
             onClick={() => toggleOpen()}
-            className="fixed right-6 top-8 z-50 flex h-12 w-12 items-center justify-center rounded-full border border-neutral-700/50 bg-[#131010]/40 text-white shadow-[inset_0px_0.0625rem_0_rgba(255,255,255,0.05),0_0.25rem_0.5rem_0_rgba(0,0,0,0.1)] filter backdrop-blur"
+            className="sticky left-0 top-0 z-30 flex h-14 w-full items-center justify-between border-b border-neutral-700/50 bg-[#131010]/40 px-6 text-white shadow-[inset_0px_0.0625rem_0_rgba(255,255,255,0.05),0_0.25rem_0.5rem_0_rgba(0,0,0,0.1)] filter backdrop-blur"
          >
+            <h3 className="font-general-sans text-base font-bold">
+               tony&apos;s space
+            </h3>
             <Hamburger toggled={isOpen} size={20} color="#e5e7eb" />
          </button>
       </>
