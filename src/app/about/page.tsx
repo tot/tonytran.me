ece083393c59e06984c7c1ebbcde1096fa998acb
Tony <totran0331@gmail.com>
2023-09-11 02:06:41 -0400

    updated about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@
 export default function Blog() {
    return (
       <main className="relative mx-auto flex min-h-screen max-w-4xl flex-col">
-         <h1 className="mt-12 text-center font-general-sans text-3xl font-bold text-neutral-100">
+         <h1 className="pt-12 text-center font-general-sans text-3xl font-bold text-neutral-100 md:pt-24">
             Page is under construction!
          </h1>
       </main>
