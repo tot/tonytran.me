8a8cef147ea01a42f3342ff1b7afdd154c09d008
Tony <totran0331@gmail.com>
2023-09-11 01:53:58 -0400

    Temporary about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,9 @@
 export default function Blog() {
    return (
       <main className="relative mx-auto flex min-h-screen max-w-4xl flex-col">
-         <div className="fixed bottom-0 left-0 right-0 top-0 -z-10 bg-[linear-gradient(to_right,#4f4f4f1A_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f1A_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+         <h1 className="mt-12 text-center font-general-sans text-3xl font-bold text-neutral-100">
+            Page is under construction!
+         </h1>
       </main>
    )
 }
