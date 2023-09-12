d002dab73cb45c267b9103a40479512e9be0e46a
Tony <totran0331@gmail.com>
2023-09-12 17:33:33 -0400

    Completed link components

--- src/app/components/mdx/a.tsx
+++ src/app/components/mdx/a.tsx
@@ -1,14 +1,33 @@
 import Link from "next/link"
 import { HTMLProps } from "react"
-import { BiLinkExternal } from "react-icons/bi"
+import { FiArrowUpRight } from "react-icons/fi"
 
 const a = ({ href, children }: HTMLProps<HTMLAnchorElement>) => {
    if (href && href.startsWith("/")) {
-      return <Link href={href}>{children}</Link>
+      return (
+         <Link href={href}>
+            <div className="group inline-flex items-start underline underline-offset-2 transition-colors duration-150 hover:decoration-[#62fde8] focus:bg-[#62fde8]/10">
+               <span className="transition-colors duration-150 group-hover:text-[#62fde8]">
+                  {children}
+               </span>
+            </div>
+         </Link>
+      )
    }
 
    if (href && href.startsWith("#")) {
-      return <a href={href}>{children}</a>
+      return (
+         <a
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="group inline-flex items-start transition-colors duration-150"
+         >
+            <span className="transition-colors duration-150 group-hover:text-[#62fde8]">
+               {children}
+            </span>
+         </a>
+      )
    }
 
    return (
@@ -16,12 +35,12 @@ const a = ({ href, children }: HTMLProps<HTMLAnchorElement>) => {
          href={href}
          target="_blank"
          rel="noopener noreferrer"
-         className="font-medium text-[#62fde8]"
+         className="group inline-flex items-start underline underline-offset-2 transition-colors duration-150 hover:decoration-[#62fde8] focus:bg-[#62fde8]/10"
       >
-         {children}
-         <div className="inline self-start pl-1">
-            <BiLinkExternal className="inline" />
-         </div>
+         <span className="transition-colors duration-150 group-hover:text-[#62fde8]">
+            {children}
+         </span>
+         <FiArrowUpRight className="mt-0.5 h-5 w-5 text-neutral-500 transition-colors  duration-150 group-hover:text-[#62fde8]" />
       </a>
    )
 }
