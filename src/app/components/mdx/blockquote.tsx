33d8c7466318cbe199f6d5199c92d5871eb88787
Tony <totran0331@gmail.com>
2023-09-12 11:34:00 -0400

    Added icon to blockquote

--- src/app/components/mdx/blockquote.tsx
+++ src/app/components/mdx/blockquote.tsx
@@ -1,15 +1,19 @@
 import { cn } from "@/app/utils/utils"
 import { HTMLProps } from "react"
+import { BiSolidQuoteAltLeft } from "react-icons/bi"
 
 const blockquote = (props: HTMLProps<HTMLQuoteElement>) => {
    return (
       <div className="py-2">
          <blockquote
             className={cn(
-               "mdx-blockquote border-l-2 border-neutral-700 px-4 py-2 font-serif font-medium",
+               "mdx-blockquote relative border-l-2 border-neutral-700 px-8 py-4 font-serif font-medium",
                props.className
             )}
          >
+            <div className="absolute -left-4 top-1/2 flex h-8 w-8 -translate-y-1/2 items-center justify-center rounded-full border-neutral-700 bg-[#0c0b0b]">
+               <BiSolidQuoteAltLeft className="h-4 w-4 text-neutral-500" />
+            </div>
             {props.children}
          </blockquote>
       </div>
