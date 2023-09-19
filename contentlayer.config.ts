de0c63aaf31562a5d41f43db0c12b55ea68d9029
Tony <totran0331@gmail.com>
2023-09-19 01:59:51 -0400

    Replaced remote source files with local files

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,9 +1,5 @@
-import { defineDocumentType } from "contentlayer/source-files"
-import { makeSource } from "contentlayer/source-remote-files"
-import { spawn } from "node:child_process"
-import { exists, copy } from "fs-extra"
+import { makeSource, defineDocumentType } from "contentlayer/source-files"
 import readingTime from "reading-time"
-import { s } from "hastscript"
 import rehypePrettyCode, {
    type Options as PrettyCodeOptions,
 } from "rehype-pretty-code"
@@ -11,9 +7,6 @@ import { fileURLToPath } from "node:url"
 import path from "path"
 import fs from "fs"
 import remarkGfm from "remark-gfm"
-
-const SYNC_ASSETS_FOLDER = `./${process.env.REPO_NAME}/published/assets`
-const PUBLIC_ASSETS_FOLDER = "./public/assets/blog"
 
 export const Post = defineDocumentType(() => ({
    name: "Post",
@@ -44,78 +37,8 @@ export const Post = defineDocumentType(() => ({
    },
 }))
 
-const runBashCommand = (command: string) =>
-   new Promise(async (resolve, reject) => {
-      const child = spawn(command, [], { shell: true })
-
-      child.stdout.setEncoding("utf8")
-      child.stdout.on("data", (data) => process.stdout.write(data))
-      child.stderr.setEncoding("utf8")
-      child.stderr.on("data", (data) => process.stderr.write(data))
-
-      child.on("close", function (code) {
-         if (code === 0) {
-            resolve(void 0)
-         } else {
-            reject(new Error(`Command failed with exit code ${code}`))
-         }
-      })
-   })
-
-const syncImages = async () => {
-   try {
-      await copy(SYNC_ASSETS_FOLDER, PUBLIC_ASSETS_FOLDER)
-      console.log("Synced blog assets!")
-   } catch (e) {
-      console.error(e)
-   }
-}
-
-const syncContentFromGit = async (contentDir: string) => {
-   const syncRun = async () => {
-      const gitUrl = `https://${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_USER}/${process.env.REPO_NAME}.git`
-
-      const isDirExists = await exists("blog-posts")
-
-      try {
-         if (isDirExists) {
-            await runBashCommand(`cd "${contentDir}" && git pull`)
-         } else {
-            await runBashCommand(
-               `git clone --depth 1 --single-branch ${gitUrl} ${contentDir}`
-            )
-         }
-         await syncImages()
-      } catch (e) {
-         console.error(e)
-      }
-   }
-
-   let wasCancelled = false
-   let syncInterval: NodeJS.Timeout
-
-   const syncLoop = async () => {
-      console.log("Syncing content files from git")
-
-      await syncRun()
-
-      if (wasCancelled) return
-
-      syncInterval = setTimeout(syncLoop, 1000 * 60)
-   }
-
-   // Block until the first sync is done
-   await syncLoop()
-
-   return () => {
-      wasCancelled = true
-      clearTimeout(syncInterval)
-   }
-}
-
 export default makeSource({
-   syncFiles: syncContentFromGit,
-   contentDirPath: `posts`,
+   contentDirPath: `posts/published`,
    documentTypes: [Post],
    disableImportAliasWarning: true,
    mdx: {
