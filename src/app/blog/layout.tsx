90edcbc65e4e32ae30702d698aa823e5d380beab
Tony <totran0331@gmail.com>
2023-09-20 00:24:02 -0400

    Removed footer

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from "react"
-import Footer from "../components/Footer"
 
 interface BlogLayoutProps {
    children: ReactNode
@@ -7,11 +6,8 @@ interface BlogLayoutProps {
 
 export default function BlogLayout({ children }: BlogLayoutProps) {
    return (
-      <div className="mx-auto flex min-h-screen w-full max-w-[50rem] flex-col justify-between px-6">
+      <div className="mx-auto flex min-h-screen w-full max-w-[50rem] flex-col justify-between px-6 pb-12">
          <div>{children}</div>
-         <div className="">
-            <Footer />
-         </div>
       </div>
    )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,14 @@
 import Writings from "./components/home/Writings"
 import Projects from "./components/home/Projects"
 import Intro from "./components/home/Intro"
-import Footer from "./components/Footer"
 
 export default function Home() {
    return (
       <div className="relative">
-         {/* <div className="absolute left-0 top-0 h-screen w-full bg-transparent bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.08)_0,rgba(0,163,255,0.09)_50%,rgba(12,11,11,0.07)_100%)]"></div> */}
          <main className="relative mx-auto flex min-h-screen max-w-[50rem] flex-col">
-            {/* <div className="fixed -top-10 bottom-0 left-0 right-0 -z-10 bg-[linear-gradient(to_right,#4f4f4f1A_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f1A_1px,transparent_1px)] bg-[size:24px_24px]"></div> */}
             <Intro />
             <Writings />
             <Projects />
-            <Footer />
          </main>
       </div>
    )
