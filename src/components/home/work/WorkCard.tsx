873af2df491b7d482e7aa0f723a5e9cc676da76b
Tony <totran0331@gmail.com>
2024-10-02 23:03:36 -0400

    Finish work section

--- src/components/home/work/WorkCard.tsx
+++ src/components/home/work/WorkCard.tsx
@@ -16,29 +16,31 @@ const WorkCard = ({
     first,
 }: WorkCardProps) => {
     return (
-        <div
-            className={`grid grid-cols-12 ${first ? "border-t border-white/10" : ""}`}
-        >
-            <div className={`col-span-4 border-b border-white/10`}>
-                <div className="p-4 h-full border-r border-white/10">
-                    <p className="text-white/30 text-sm font-departure tracking-wider">
+        <div className={`grid grid-cols-12`}>
+            <div
+                className={`col-span-12 md:col-span-4 md:border-b border-white/10`}
+            >
+                <div className="px-6 pt-6 pb-3 md:p-6 h-full border-r border-white/10">
+                    <p className="text-white/50 text-sm font-departure tracking-wider">
                         {date}
                     </p>
                 </div>
             </div>
-            <div className={`col-span-8`}>
-                <div className="p-4 space-y-2 border-r border-white/10">
+            <div className={`col-span-12 md:col-span-8`}>
+                <div className="px-6 pb-6 md:p-6 space-y-2 border-r border-white/10">
                     <p className="text-white font-departure font-medium tracking-wide">
                         {title} &mdash; {company}
                     </p>
-                    <p className="text-white/60">{description}</p>
+                    <p className="text-white/60 text-sm leading-6">
+                        {description}
+                    </p>
                 </div>
                 <div className="border-t border-white/10">
-                    <ul className="flex flex-wrap text-sm font-departure tracking-wider text-white/30">
+                    <ul className="flex flex-wrap text-xs font-departure tracking-wider text-white/30">
                         {technologies.map((technology, index) => (
                             <li
                                 key={index}
-                                className="py-2 px-4 border-r border-b border-white/10 flex-grow"
+                                className="py-3 px-6 border-r border-b border-white/10 flex-grow"
                             >
                                 {technology}
                             </li>
