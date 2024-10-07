72bd1053fcf41370f8f3d7a5e6bd0cc9ad166816
Tony <totran0331@gmail.com>
2024-10-07 15:14:35 -0400

    Add photo lightbox component

---
+++ src/components/gallery/LightboxWrapper.tsx
@@ -0,0 +1,53 @@
+import React, { useState, useRef, useEffect } from "react";
+import Lightbox, { type Slide } from "yet-another-react-lightbox";
+import Inline from "yet-another-react-lightbox/plugins/inline";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+
+interface LightboxWrapperProps {
+    children: React.ReactNode;
+    slide: Slide;
+    imageId: string;
+}
+
+export default function LightboxWrapper({
+    children,
+    slide,
+    imageId,
+}: LightboxWrapperProps) {
+    const [open, setOpen] = useState(false);
+    const zoomRef = useRef(null);
+
+    useEffect(() => {
+        const handleToggle = (event: any) => {
+            if (event.detail.id === imageId) {
+                setOpen(!open);
+            }
+        };
+        document.addEventListener("toggleLightbox", handleToggle);
+        return () =>
+            document.removeEventListener("toggleLightbox", handleToggle);
+    }, [open, imageId]);
+
+    return (
+        <>
+            {children}
+            <Lightbox
+                open={open}
+                close={() => setOpen(false)}
+                slides={[slide]}
+                plugins={[Zoom]}
+                zoom={{ ref: zoomRef }}
+                controller={{
+                    closeOnBackdropClick: true,
+                }}
+                carousel={{
+                    finite: true,
+                }}
+                render={{
+                    buttonPrev: () => null,
+                    buttonNext: () => null,
+                }}
+            />
+        </>
+    );
+}
