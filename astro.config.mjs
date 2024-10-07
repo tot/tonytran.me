b3bea17f434ea973a01ac85a966088ae3916fe52
Tony <totran0331@gmail.com>
2024-10-07 15:11:22 -0400

    Add mdx rehype plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,21 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [react(), tailwind(), mdx(), sitemap(), icon()],
+    markdown: {
+        remarkPlugins: [rehypeHeadingIds],
+        // rehypePlugins: [
+        //     rehypeSlug,
+        //     [rehypeAutolinkHeadings, { behavior: "after" }],
+        // ],
+    },
 });
