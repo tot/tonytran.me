9dfefe3379e49dc4572c95804230297926876d38
Tony <totran0331@gmail.com>
2024-11-04 15:55:17 -0500

    Refactor getProjectImages function to handle missing metadata.json

--- src/modules/project.ts
+++ src/modules/project.ts
@@ -11,27 +11,32 @@ export const getProjectImages = async (projectId: string) => {
         Object.entries(images).filter(([key]) => key.includes(projectId)),
     );
 
-    const metadataPath = `/src/content/projects/_images/${projectId}/metadata.json`;
-    const metadata = await import(/* @vite-ignore */ metadataPath);
+    try {
+        const metadataPath = `/src/content/projects/_images/${projectId}/metadata.json`;
+        const metadata = await import(/* @vite-ignore */ metadataPath);
 
-    // 3. Images are promises, so we need to resolve the glob promises
-    const resolvedImages = await Promise.all(
-        Object.values(images).map(async (image) => {
-            const mod = await image();
-            const filename = mod.default.src.split("/").pop() as string;
-            // Find the image metadata
-            const imageMetadata = metadata.images.find((img: ImageInfo) =>
-                filename.includes(img.filename),
-            );
-            return {
-                ...mod.default,
-                // Add the image metadata
-                ...imageMetadata,
-            };
-        }),
-    );
+        // 3. Images are promises, so we need to resolve the glob promises
+        const resolvedImages = await Promise.all(
+            Object.values(images).map(async (image) => {
+                const mod = await image();
+                const filename = mod.default.src.split("/").pop() as string;
+                // Find the image metadata
+                const imageMetadata = metadata.images.find((img: ImageInfo) =>
+                    filename.includes(img.filename),
+                );
+                return {
+                    ...mod.default,
+                    // Add the image metadata
+                    ...imageMetadata,
+                };
+            }),
+        );
 
-    // 4. Sort images in order
-    resolvedImages.sort((a, b) => a.order - b.order);
-    return resolvedImages;
+        // 4. Sort images in order
+        resolvedImages.sort((a, b) => a.order - b.order);
+        return resolvedImages;
+    } catch (error) {
+        // If metadata.json doesn't exist, return an empty array
+        return [];
+    }
 };
